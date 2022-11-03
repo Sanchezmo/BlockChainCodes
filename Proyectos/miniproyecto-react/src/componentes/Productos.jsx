c614d687b37abbce13351fdec8eac977d0c6d068
Sanchezmo <psmoran87@gmail.com>
2022-11-03 19:02:37 +0100

    fin proyecto hasta productos

--- Proyectos/miniproyecto-react/src/componentes/Productos.jsx
+++ Proyectos/miniproyecto-react/src/componentes/Productos.jsx
@@ -1,20 +1,20 @@
 import {useQuery} from "react-query"
-import {axios} from "axios"
+import axios from "axios"
 
 function getProductos(){
     return axios.get('http://localhost:8080/sql?sql=select * from products')
 }
 
 export function Productos(){
-    const {data:productos,isLoading,isError}=useQuery(["productos"],getProductos)
+    const {data:productos,isLoading}=useQuery(["productos"],getProductos)
     
     if(isLoading){
         return <div>cargando...</div>
     }
    
     return(
-     //<div>{JSON.stringify(productos)}</div>
-     <table>
+    // <div>{JSON.stringify(productos)}</div>
+    <table className="table">
         <thead>
         <tr>
             
@@ -28,7 +28,7 @@ export function Productos(){
                 <tr key={productos.product_id}>
                     <td>{productos.product_id}</td>
                     <td>{productos.product_name}</td>
-                    <td>{productos.product_price}</td>
+                    <td>{productos.unit_price}</td>
                 </tr>
             ))}
         </tbody>
---
+++ web server bd/index.js
@@ -0,0 +1,39 @@
+var express = require("express")
+var cors = require("cors")
+var bodyParser = require("body-parser")
+require('dotenv').config()
+
+var app = express();
+app.use(cors())
+var conString = process.env.CONN;
+var pg = require("pg");
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get("/ping", (req, res) => {
+    res.send(new Date().toISOString());
+
+});
+app.get("/", (req, res) => {
+    res.send("Hello World22");
+});
+
+app.get("/sql", async (req, res) => {
+    
+    var client =new pg.Client({
+        host: "localhost",
+        port: 5432,
+        database: "postgres",
+        user: "postgres",
+        password: "mysecretkey"
+        
+    })
+    await client.connect()
+    const filas = await client.query(req.query.sql, [])
+    await client.end()
+    res.send(filas.rows);
+});
+
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => console.log(`Server is running in port ${PORT}`));
