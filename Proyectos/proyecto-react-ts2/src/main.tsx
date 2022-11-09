19f13be956c42b2a2277b290bc0e44cf6e21a00c
Sanchezmo <psmoran87@gmail.com>
2022-11-10 00:47:27 +0100

    react query

--- Proyectos/proyecto-react-ts2/src/main.tsx
+++ Proyectos/proyecto-react-ts2/src/main.tsx
@@ -1,10 +1,26 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import {QueryClientProvider,QueryClient,useQuery} from 'react-query' 
+interface IAppProps{
+  id:number
+}
+const App: React.FC<IAppProps> = ({id}) =>{
+  const {data,isLoading}=useQuery(["post"], async() =>{
+    const datos = await (await fetch(`https://jsonplaceholder.typicode.com/posts/${id}`)).json()
+    return datos
+  })
+  if (isLoading) return <p>Esta cargando</p>
+  return (
+    <div>
+      <p>{JSON.stringify(data)}</p>
+    </div>
+  )
+}
+//implementation of the queryClient
+const queryClient= new QueryClient()
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+ <QueryClientProvider client={queryClient}>
+ <App id={10}></App>
+ </QueryClientProvider>
 )
