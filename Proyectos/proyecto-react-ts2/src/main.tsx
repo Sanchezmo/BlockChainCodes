c2e0bc0757473fd73ed66b331c974af535a367ba
Sanchezmo <psmoran87@gmail.com>
2022-11-10 01:38:41 +0100

    fin >React

--- Proyectos/proyecto-react-ts2/src/main.tsx
+++ Proyectos/proyecto-react-ts2/src/main.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import {QueryClientProvider,QueryClient,useQuery} from 'react-query' 
+import {Routes,BrowserRouter,Route,Link,Outlet} from 'react-router-dom'
 interface IAppProps{
   id:number
 }
@@ -14,13 +15,37 @@ const App: React.FC<IAppProps> = ({id}) =>{
     <div>
       <p>{JSON.stringify(data)}</p>
     </div>
-  )
+ )
 }
-//implementation of the queryClient
+//implementation of the queryClient <App id={10}></App>
 const queryClient= new QueryClient()
 
+const Layout = ()=>{
+  return (<>
+    <div>
+      <p><Link to="/app">| App | </Link>
+      <Link to="/aboutus">About Us | </Link>
+     <Link to="/servicios">Servicios| </Link>
+     <Link to="/contacto">Contacto |</Link></p>
+    </div>
+    <div>
+      <Outlet></Outlet>
+    </div>
+    </>
+  )
+}
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+  <BrowserRouter>
  <QueryClientProvider client={queryClient}>
- <App id={10}></App>
- </QueryClientProvider>
+  <Routes>
+    <Route path="/" element={<Layout></Layout>}>
+     <Route path="*" element={"pagina no encontrada error 404"}/>
+     <Route path="/app" element={<App id={10}></App>}/>
+     <Route path="/contacto" element={"Contacto"}/>
+     <Route path="/aboutus" element={"sobre nosotros"}/>
+     <Route path="/servicios" element={"Servicios"}/>
+    </Route>
+  </Routes>
+    </QueryClientProvider>
+ </BrowserRouter>
 )
